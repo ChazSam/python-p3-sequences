253a9d7c9354657c9d68cd66f9bf3d88d5810d49
Charles <Charlessammarco@gmail.com>
2023-11-16 20:43:43 -0600

    fibonacci

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    i = 0
+    j = 1
+    count = 1
+    my_list = []
+
+
+    if length == 0:
+        print(my_list)
+    elif length == 1:
+         my_list.append(i)
+         print(my_list)
+    else:    
+        my_list=[i]
+        while count < length:
+                # my_list=[i]
+                count += 1
+                i, j = j, i + j
+                my_list.append(i)
+        print(my_list)
+
+# print_fibonacci(1)
